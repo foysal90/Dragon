3a7aae8504b73e9f1f21030a9ab8658493829f6f
foysal90 <foysal1416@hotmail.com>
2022-04-08 23:45:44 -0400

    Draggon Game

---
+++ IdeaProjects/Dragon/src/com/example/dragon/Dragon.java
@@ -0,0 +1,23 @@
+package com.example.dragon;
+import java.util.*;
+
+public class Dragon {
+    public static void main(String[] args) {
+
+                Scanner sc= new Scanner(System.in);
+                System.out.println("You are in a land full of dragons, In front of you, you see two caves. In one cave, the dragon is friendly and will share his treasure with you. The other dragon is greedy and hungry and will eat you on sight. Which cave will you go into?(1 or 2)");
+                int a= sc.nextInt();
+                if (a==1){
+                    System.out.println("You approach the cave... \n It is dark and spooky... \n A large dragon jumps out in front of you! He opes his jaws and... \nGobbles you down in one bite!");
+                }
+                else{
+                    System.out.println("The dragon is friendly and shares the loot with you");
+
+
+        }
+    }
+}
+
+
+
+
